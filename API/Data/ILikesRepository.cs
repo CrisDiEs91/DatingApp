727056b6ffb9bcb38e700d8d476b3117df81882d
Cristina Díaz Esquivel <cristinaesquivel959@gmail.com>
2025-03-23 09:48:00 -0600

    Parcial05 - Add pagination for Likes

--- API/Data/ILikesRepository.cs
+++ API/Data/ILikesRepository.cs
@@ -7,7 +7,7 @@ public interface ILikesRepository
 {
     public Task<UserLike?> GetUserLikeAsync(int sourceUserId, int targerUserId);
     public Task<IEnumerable<MemberResponse>> GetUserLikesAsync(string predicate, int userId);
-    public Task<IEnumerable<int>> GetCurrentUserLikeIdsAsync(int currentUSerId);
+    public Task<PagedList<MemberResponse>> GetUserLikesAsync(LikesParams likesParams);
     public void RemoveLike(UserLike like);
     public void AddLike(UserLike like);
     public Task<bool> SaveChangesAsync();
